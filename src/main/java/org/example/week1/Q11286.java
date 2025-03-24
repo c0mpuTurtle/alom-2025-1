4adffc119efa8df835ee752f72181579d3320f85
hyebin <shin378378@naver.com>
2025-03-24 13:32:17 +0900

    Refactor : Scanner 말고 BufferedReader쓰기

--- src/main/java/org/example/week1/Q11286.java
+++ src/main/java/org/example/week1/Q11286.java
@@ -1,12 +1,13 @@
 package org.example.week1;
 
+import java.io.*;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
 public class Q11286 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int size = Integer.parseInt(br.readLine());
 
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((n1, n2) -> {
             int abs1 = Math.abs(n1);
@@ -16,20 +17,18 @@ public class Q11286 {
         });
 
         for (int i = 0; i < size; i++) {
-            int n = scanner.nextInt();
+            int n = Integer.parseInt(br.readLine());
             if (n == 0) {
-                outputQueue(priorityQueue);
-            }
-            else{
+                if (priorityQueue.isEmpty()) {
+                    bw.write("0\n");
+                } else {
+                    bw.write(priorityQueue.poll() + "\n");
+                }
+            } else {
                 priorityQueue.add(n);
             }
         }
 
-        scanner.close();
-    }
-
-    private static void outputQueue(PriorityQueue<Integer> priorityQueue){
-        if (priorityQueue.size() != 0) System.out.println(priorityQueue.poll());
-        else System.out.println("0");
+        bw.flush();
     }
 }
